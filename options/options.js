3e391b543fba47c4cd99c3e4966b9cb87e44cfd1
Crowdbotics <team@crowdbotics.com>
2022-07-16 23:49:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "astromeet_36224",
+  url: "https://astromeet_36224.botics.co",
+  api: "https://astromeet_36224.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
